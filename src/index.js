a3257881c52e9c3d7c37c917c4e2b72206571ced
J3 <j3@discloud.com>
2026-07-30 20:35:10 +0300

    fix: add syncAdminConfigFromMongo to retry block so admin config is synced from MongoDB when it connects after startup

--- src/index.js
+++ src/index.js
@@ -268,11 +268,13 @@ async function initialize() {
         initEmbedModel();
         initAutoReplyModel();
         initReactModel();
+        initAdminModel();
         await ensureConfigLoaded();
         const { syncJsonToMongo: syncEmbeds } = require('./utils/embedStorage');
         await syncEmbeds();
         await syncAr();
         await syncRr();
+        if (syncAdminConfigFromMongo) { await syncAdminConfigFromMongo(); }
       } else {
         try {
           const { connectDatabase } = require('./utils/database');
